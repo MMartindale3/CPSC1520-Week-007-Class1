fd2236b8f015c173979c71624755c8e8328ce74a
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-02-27 19:04:04 -0700

    First Commit

---
+++ js/index.js
@@ -0,0 +1,77 @@
+/* 
+ 
+ Design Patterns
+Singleton
+Factory
+Observer 
+Decorator 
+Adapter 
+Facade 
+Strategy 
+Prototype 
+Command 
+Module
+ 
+
+
+
+Products
+Lumpfish Black
+Raspberry Jam
+Wine Sauvignon Blanc Oyster
+Jicama
+Cookie Dough Variety
+Split Peas Green, Dry
+Potatoes Idaho 80 Count
+Cake Circle, Paprus
+Pomegranates
+Pound Cake
+
+Fruits 
+apple
+banana
+orange
+strawberry
+kiwi
+pineapple
+mango
+pear
+peach 
+plum
+
+```*/
+
+/* 
+    Creating an array
+    array delclaration making an array
+
+    Array Data Types 
+    values stored in an array
+    boolean, numbers, strings, null, undefined, functions, objects
+    
+    Access an item in the array
+    array[indexNumber]
+
+    Array.length
+    length property is the number of items in the array
+
+     Querying The Dom
+     querySelectorAll()
+     querySelector()
+     
+     NodeList
+     A NodeList is a collection of document nodes
+     Examples Element Nodes, Attribute Nodes, and Text Nodes 
+
+    
+      HTMLCollection
+      An HTMLCollection is a collection of document elements
+      Element is a specific type of Node that represents an HTML element
+      Not all Nodes are Elements
+      There are different types of Nodes.
+
+
+     Children 
+      
+
+*/
