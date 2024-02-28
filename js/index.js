31e5e958a3b5f86a8842336b763956c780495a13
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-02-27 21:10:40 -0700

    Completed in class learning

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-/* 
+/* Lists of content
+
+TODO:
+Create List for all of the design patterns
+Create a render function using the template
+Heading is the data
  
  Design Patterns
 Singleton
@@ -55,6 +60,8 @@ plum
     Array.length
     length property is the number of items in the array
 
+    Array.forEach()
+
      Querying The Dom
      querySelectorAll()
      querySelector()
@@ -71,7 +78,61 @@ plum
       There are different types of Nodes.
 
 
-     Children 
+     Children (property for nodelist and html collection)
       
 
 */
+
+/*
+Fruits 
+apple
+banana
+orange
+strawberry
+kiwi
+pineapple
+mango
+pear
+peach 
+plum
+*/
+
+// create an array [] think array(?)
+// Templating - Display Templating
+// Data array... will eventually come from an external source
+// Rendering
+
+/*
+    1. Array of data
+    2. Template HTML+CSS
+    3. Render function holds the template
+    4. Loop through each item in the array
+        Array.forEach(cb)
+*/
+// const fruitsList = ['apples', 'banana', 'kiwi', 'pear', 'orange']
+
+// fruitsList.forEach((fruit) => {
+//     render(fruit, document.querySelector('#fruits'))
+// })
+
+// function render(data, domContainer) {
+//     const template = `<li class="text-lg text-zinc-600 border-b border-zinc-300 py-1 px-4">${data}</li>`
+//     // afterbegin  place first last
+//     //before end  place the first element first
+//     domContainer.insertAdjacentHTML('beforeend', template)
+// }
+
+// layout thrashing
+const patternsList = ["Singleton", "Factory", "Observer", "Decorator", "Adapter", "Facade", "Strategy", "Prototype", "Command", "Module"]
+
+patternsList.forEach((pattern) => {
+        render(pattern, document.querySelector('#patterns'))
+    })
+
+function render(data, container) {
+    const template = `<li class="border py-8 px-6">
+    <h3 class="text-lg font-semibold underline font-zinc-950">${data}</h3>
+    <p class="text-sm text-zinc-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus error velit doloremque?</p>
+</li>`
+    container.insertAdjacentHTML('beforeend', template)
+}
